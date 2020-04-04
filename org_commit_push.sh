cd3b7c2b10f18040654d8a89a693b645ec7fe604
Benson Chu <bensonchu457@gmail.com>
2020-04-04 17:35:01 -0500

    mobile branch will be unsafe

--- org_commit_push.sh
+++ org_commit_push.sh
@@ -4,7 +4,7 @@ if ! git diff --quiet
 then
     git add .
     git commit -m "Mobile autocommit: $(date +'%D %T')"
-    git push -u origin mobile
+    git push --force -u origin mobile
 else
     git pull --ff-only
 fi
