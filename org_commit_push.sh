328c19baab6047f7fb05ccd9c45c49c70284fb37
Benson Chu <bensonchu457@gmail.com>
2020-04-04 17:17:55 -0500

    Conditional

--- org_commit_push.sh
+++ org_commit_push.sh
@@ -1,4 +1,8 @@
 cd /sdcard/agenda
-git add .
-git commit -m "Mobile autocommit: $(date +'%D %T')"
-git push -u origin mobile
+
+if ! git diff --quiet
+then
+    git add .
+    git commit -m "Mobile autocommit: $(date +'%D %T')"
+    git push -u origin mobile
+fi
