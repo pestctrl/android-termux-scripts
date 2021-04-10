43f0a9f6f0dcb7ef17c46d7c4a24de1e077c70f2
Benson Chu <bensonchu457@gmail.com>
2021-04-10 15:10:58 -0500

    Push back regardless

--- org_commit_push.sh
+++ org_commit_push.sh
@@ -4,9 +4,10 @@ if ! git diff --quiet
 then
     git add .
     git commit -m "Mobile autocommit: $(date +'%D %T')"
-    git push --force -u origin mobile
 else
     git fetch --all
     git pull --ff-only
     git merge origin/desktop --ff-only
 fi
+
+git push --force -u origin mobile
