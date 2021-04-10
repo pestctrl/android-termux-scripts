38987678ade780c012b14318a3754837872cba16
Benson Chu <bensonchu457@gmail.com>
2021-04-10 15:02:10 -0500

    Persist reboot, merge with desktop if possible

--- org_commit_push.sh
+++ org_commit_push.sh
@@ -6,5 +6,7 @@ then
     git commit -m "Mobile autocommit: $(date +'%D %T')"
     git push --force -u origin mobile
 else
+    git fetch --all
     git pull --ff-only
+    git merge origin/desktop --ff-only
 fi
--- schedule_org.sh
+++ schedule_org.sh
@@ -1,2 +1,1 @@
-termux-job-scheduler -s "/data/data/com.termux/files/home/android-termux-scripts/org_commit_push.sh" --period-ms 300000 --network any
-    
+termux-job-scheduler -s "/data/data/com.termux/files/home/android-termux-scripts/org_commit_push.sh" --period-ms 300000 --network unmetered --persisted true
