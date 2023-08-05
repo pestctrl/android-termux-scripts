1ce6f4e4189d9616a99b6a900fde17512f4f8628
Benson Chu <bensonchu457@gmail.com>
2023-08-05 09:58:56 -0500

    Error messages

--- schedule_org.sh
+++ schedule_org.sh
@@ -1,1 +1,8 @@
+if ! command -v termux-job-scheduler &> /dev/null
+then
+    echo "Couldn't find termux-job-scheduler"
+    echo "Make sure to install the termux:api package, and run 'pkg install termux-api'"
+    exit -1
+fi
+
 termux-job-scheduler -s "/data/data/com.termux/files/home/android-termux-scripts/org_commit_push.sh" --period-ms 300000 --network unmetered --persisted true
